f97bc11254b15d7cdf492817a38ac1e6a6c40923
Napat0000 <88538930+Napat0000@users.noreply.github.com>
2022-12-07 19:45:56 +0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -47,22 +47,73 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     if (!(Hdir == 0)) {
+        Head.setImage(img`
+            d d d d d d d b 
+            d b b b b b b c 
+            d b b b b b b c 
+            d b b b b b b c 
+            d b b b b b b c 
+            d 1 1 b b 1 1 c 
+            d 1 f b b f 1 c 
+            b c c c c c c c 
+            `)
         Hdir = 180
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Released, function () {
     if (!(Hdir == 270)) {
+        Head.setImage(img`
+            d d d d d d d b 
+            d b b b b 1 1 c 
+            d b b b b 1 f c 
+            d b b b b b b c 
+            d b b b b b b c 
+            d b b b b 1 f c 
+            d b b b b 1 1 c 
+            b c c c c c c c 
+            `)
         Hdir = 90
     }
 })
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     if (!(Hdir == 90)) {
+        Head.setImage(img`
+            d d d d d d d b 
+            d 1 1 b b b b c 
+            d f 1 b b b b c 
+            d b b b b b b c 
+            d b b b b b b c 
+            d f 1 b b b b c 
+            d 1 1 b b b b c 
+            b c c c c c c c 
+            `)
         Hdir = 270
     }
 })
 sprites.onOverlap(SpriteKind.Head, SpriteKind.Body, function (sprite, otherSprite) {
     game.over(false)
 })
+function Go_through_in_the_wall (Head_dir: number, Switch_Id: boolean) {
+    if (Head.y <= 16 && Head_dir == 0) {
+        Head.y = scene.screenHeight() - 12
+        Go_head(Head_dir)
+        Head.y += 8
+    } else if (Head.x >= scene.screenWidth() - 8 && Head_dir == 90) {
+        Head.x = 4
+        Go_head(Head_dir)
+        Head.x += -8
+    } else if (Head.y >= scene.screenHeight() - 16 && Head_dir == 180) {
+        Head.y = 12
+        Go_head(Head_dir)
+        Head.y += -8
+    } else if (Head.x <= 16 && Head_dir == 270) {
+        Head.x = scene.screenWidth() - 4
+        Go_head(Head_dir)
+        Head.x += 8
+    } else {
+        Go_head(Head_dir)
+    }
+}
 sprites.onOverlap(SpriteKind.Body, SpriteKind.Food, function (sprite, otherSprite) {
     tiles.placeOnRandomTile(otherSprite, assets.tile`myTile0`)
 })
@@ -74,16 +125,38 @@ sprites.onOverlap(SpriteKind.Head, SpriteKind.BonusFood, function (sprite, other
     otherSprite.destroy()
     music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 1728, 2526, 0, 255, 150, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
 })
+function Go_head (Dir: number) {
+    if (Dir == 0) {
+        Head.y += -8
+    } else if (Dir == 90) {
+        Head.x += 8
+    } else if (Dir == 180) {
+        Head.y += 8
+    } else {
+        Head.x += -8
+    }
+}
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     if (!(Hdir == 180)) {
+        Head.setImage(img`
+            d d d d d d d b 
+            d 1 f b b f 1 c 
+            d 1 1 b b 1 1 c 
+            d b b b b b b c 
+            d b b b b b b c 
+            d b b b b b b c 
+            d b b b b b b c 
+            b c c c c c c c 
+            `)
         Hdir = 0
     }
 })
-scene.onOverlapTile(SpriteKind.Head, assets.tile`myTile`, function (sprite, location) {
-    if (Head.tileKindAt(TileDirection.Center, assets.tile`myTile`)) {
-        game.over(false)
-    }
-})
+function Try_clone () {
+    Bx.unshift(Head.x)
+    By.unshift(Head.y)
+    Bdir.unshift(Hdir)
+}
+let CloneID = 0
 let BonusBar: StatusBarSprite = null
 let Bonusnumscore = 0
 let Bonus: Sprite = null
@@ -94,17 +167,20 @@ let Numscore = 0
 let Snakelenght = 0
 let Eated = false
 let Hdir = 0
+let Bdir: number[] = []
+let Bx: number[] = []
+let By: number[] = []
 let Body: Sprite[] = []
 let Head: Sprite = null
 tiles.loadMap(tiles.createSmallMap(tilemap`level1`))
 Head = sprites.create(img`
     d d d d d d d b 
+    d b b b b 1 1 c 
+    d b b b b 1 f c 
     d b b b b b b c 
-    d b b c c b b c 
-    d b c b b d b c 
-    d b c b b d b c 
-    d b b d d b b c 
     d b b b b b b c 
+    d b b b b 1 f c 
+    d b b b b 1 1 c 
     b c c c c c c c 
     `, SpriteKind.Head)
 Body = [sprites.create(img`
@@ -130,18 +206,15 @@ let Squarefood = sprites.create(img`
     2 e e e e e e e 
     `, SpriteKind.Food)
 tiles.placeOnRandomTile(Squarefood, assets.tile`myTile0`)
-let By: number[] = []
-let Bx: number[] = []
-let Bdir: number[] = []
+By = []
+Bx = []
+Bdir = []
 Hdir = 90
 let Max_length = 6
 Eated = false
 Snakelenght = 1
 music.setVolume(255)
 Score_setup()
-Bx.unshift(Head.x)
-By.unshift(Head.y)
-Bdir.unshift(Hdir)
 forever(function () {
     if (Snakelenght >= Max_length) {
         Eated = false
@@ -171,7 +244,7 @@ forever(function () {
             ........4eeeeeeeeeeeeeee
             ........eeeeeeeeeeeeeeee
             `, SpriteKind.BonusFood)
-        tiles.placeOnTile(Bonus, tiles.getTileLocation(randint(1, 17), randint(1, 12)))
+        tiles.placeOnTile(Bonus, tiles.getTileLocation(randint(0, 18), randint(1, 12)))
         Bonusnumscore = randint(40, 50)
         BonusBar = statusbars.create(160, 8, StatusBarKind.BonusBar)
         BonusBar.setPosition(80, scene.screenHeight() - 4)
@@ -196,18 +269,8 @@ forever(function () {
     }
 })
 forever(function () {
-    if (Hdir == 0) {
-        Head.y += -8
-    } else if (Hdir == 90) {
-        Head.x += 8
-    } else if (Hdir == 180) {
-        Head.y += 8
-    } else {
-        Head.x += -8
-    }
-    Bx.unshift(Head.x)
-    By.unshift(Head.y)
-    Bdir.unshift(Hdir)
+    Try_clone()
+    Go_through_in_the_wall(Hdir, false)
     pause(250)
 })
 forever(function () {
@@ -221,7 +284,8 @@ forever(function () {
 forever(function () {
     for (let index = 0; index <= Snakelenght; index++) {
         if (index > 0) {
-            Body[index - 1].setPosition(Bx[index], By[index])
+            CloneID = index - 1
+            Body[index - 1].setPosition(Bx[CloneID], By[CloneID])
         }
     }
 })
